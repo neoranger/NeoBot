bf3c51a163ceb4395a25f35270b5209abf2731c3
NeoRanger <elgranpote@gmail.com>
2016-03-04 12:25:31 -0300

    Adding script for run bot on startup

---
+++ runbot.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+########################################################################
+You can edit the path to select the correct .py file for run the script
+########################################################################
+
+DIR=/home/$USER/Git/NeoBot
+cd $DIR
+python neobot.py
