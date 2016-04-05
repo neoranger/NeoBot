8fae0aa3e288ea23c53c599521e9e128175c0855
neoranger <neoranger@openmailbox.org>
2016-04-05 17:13:50 -0300

    Update functions.py
    Se movieron las imagenes a un directorio aparte.

--- functions.py
+++ functions.py
@@ -19,17 +19,17 @@ def command_kick(m):
 @bot.message_handler(commands=['uppercut'])
 def command_uppercut(m): 
     cid = m.chat.id 
-    bot.send_photo( cid, open( 'uppercut.jpg', 'rb')) 
+    bot.send_photo( cid, open( './imagenes/uppercut.jpg', 'rb')) 
  
 @bot.message_handler(commands=['hadouken']) 
 def command_hadouken(m): 
     cid = m.chat.id 
-    bot.send_photo( cid, open( 'hadouken.gif', 'rb')) 
+    bot.send_photo( cid, open( './imagenes/hadouken.gif', 'rb')) 
     
 @bot.message_handler(commands=['windowsero']) 
 def command_windowsero(m): 
     cid = m.chat.id 
-    bot.send_photo( cid, open( 'windowsero.jpg', 'rb'))
+    bot.send_photo( cid, open( './imagenes/windowsero.jpg', 'rb'))
     
 @bot.message_handler(commands=['acerca']) 
 def command_acerca(m):
@@ -54,12 +54,12 @@ def command_hello(m):
 @bot.message_handler(commands=['attack']) 
 def command_attack(m): 
     cid = m.chat.id 
-    bot.send_photo( cid, open( 'dictionary_attack.jpg', 'rb')) 
+    bot.send_photo( cid, open( './imagenes/dictionary_attack.jpg', 'rb')) 
 
 @bot.message_handler(commands=['nsa']) 
 def command_nsa(m): 
     cid = m.chat.id 
-    bot.send_photo( cid, open( 'tiosam.jpg', 'rb'))
+    bot.send_photo( cid, open( './imagenes/tiosam.jpg', 'rb'))
 
 @bot.message_handler(commands=['roll']) 
 def command_roll(m): 
@@ -78,107 +78,107 @@ def command_blogroll(m):
 @bot.message_handler(commands=['gentoo']) 
 def command_gentoo(m): 
     cid = m.chat.id 
-    bot.send_photo( cid, open( 'gentoo.jpg', 'rb')) 
+    bot.send_photo( cid, open( './imagenes/gentoo.jpg', 'rb')) 
 
 @bot.message_handler(commands=['vicman']) 
 def command_vicman(m): 
     cid = m.chat.id 
-    bot.send_photo( cid, open( 'vicman2.jpg', 'rb')) 
+    bot.send_photo( cid, open( './imagenes/vicman2.jpg', 'rb')) 
 
 @bot.message_handler(commands=['tuxamigos']) 
 def command_tuxamigos(m): 
     cid = m.chat.id 
-    bot.send_photo( cid, open( 'tuxamigos.jpg', 'rb')) 
+    bot.send_photo( cid, open( './imagenes/tuxamigos.jpg', 'rb')) 
     
 @bot.message_handler(commands=['deletethat']) 
 def command_deletethat(m): 
     cid = m.chat.id 
-    bot.send_photo( cid, open( 'deletethat.jpg', 'rb')) 
+    bot.send_photo( cid, open( './imagenes/deletethat.jpg', 'rb')) 
 
 @bot.message_handler(commands=['flame']) 
 def command_flame(m): 
     cid = m.chat.id 
-    bot.send_photo( cid, open( 'popcorn.jpg', 'rb')) 
+    bot.send_photo( cid, open( './imagenes/popcorn.jpg', 'rb')) 
 
 @bot.message_handler(commands=['stallman']) 
 def command_stallman(m): 
     cid = m.chat.id 
-    bot.send_photo( cid, open( 'dancingstallman.jpg', 'rb')) 
+    bot.send_photo( cid, open( './imagenes/dancingstallman.jpg', 'rb')) 
  
 @bot.message_handler(commands=['ok']) 
 def command_ok(m): 
     cid = m.chat.id 
-    bot.send_photo( cid, open( 'ok.jpg', 'rb')) 
+    bot.send_photo( cid, open( './imagenes/ok.jpg', 'rb')) 
 
 @bot.message_handler(commands=['coding']) 
 def command_coding(m): 
     cid = m.chat.id 
-    bot.send_photo( cid, open( 'coding.jpg', 'rb')) 
+    bot.send_photo( cid, open( './imagenes/coding.jpg', 'rb')) 
  
 @bot.message_handler(commands=['nelson']) 
 def command_nelson(m): 
     cid = m.chat.id 
-    bot.send_photo( cid, open( 'nelson.jpg', 'rb')) 
+    bot.send_photo( cid, open( './imagenes/nelson.jpg', 'rb')) 
  
 @bot.message_handler(commands=['yes']) 
 def command_fuckyeah(m): 
     cid = m.chat.id 
-    bot.send_photo( cid, open( 'fuckyeah.jpg', 'rb')) 
+    bot.send_photo( cid, open( './imagenes/fuckyeah.jpg', 'rb')) 
  
 @bot.message_handler(commands=['spoiler']) 
 def command_spoiler(m): 
     cid = m.chat.id 
-    bot.send_photo( cid, open( 'spoileralert.jpg', 'rb')) 
+    bot.send_photo( cid, open( './imagenes/spoileralert.jpg', 'rb')) 
 
 @bot.message_handler(commands=['viernes']) 
 def command_viernes(m): 
     cid = m.chat.id 
-    bot.send_photo( cid, open( 'viernes.jpg', 'rb')) 
+    bot.send_photo( cid, open( './imagenes/viernes.jpg', 'rb')) 
 
 @bot.message_handler(commands=['kde']) 
 def command_kde(m): 
     cid = m.chat.id 
-    bot.send_photo( cid, open( 'kderock.jpg', 'rb')) 
+    bot.send_photo( cid, open( './imagenes/kderock.jpg', 'rb')) 
 
 @bot.message_handler(commands=['esssta']) 
 def command_esssta(m): 
     cid = m.chat.id 
-    bot.send_photo( cid, open( 'esssta.jpg', 'rb')) 
+    bot.send_photo( cid, open( './imagenes/esssta.jpg', 'rb')) 
 
 @bot.message_handler(commands=['what']) 
 def command_what(m): 
     cid = m.chat.id 
-    bot.send_photo( cid, open( 'what.jpg', 'rb')) 
+    bot.send_photo( cid, open( './imagenes/what.jpg', 'rb')) 
  
 @bot.message_handler(commands=['vegetta']) 
 def command_vegetta(m): 
     cid = m.chat.id 
-    bot.send_photo( cid, open( 'vegetta.jpg', 'rb')) 
+    bot.send_photo( cid, open( './imagenes/vegetta.jpg', 'rb')) 
  
 @bot.message_handler(commands=['litrona']) 
 def command_litrona(m): 
     cid = m.chat.id 
-    bot.send_photo( cid, open( 'litrona.jpg', 'rb')) 
+    bot.send_photo( cid, open( './imagenes/litrona.jpg', 'rb')) 
  
 @bot.message_handler(commands=['fede']) 
 def command_fede(m): 
     cid = m.chat.id 
-    bot.send_photo( cid, open( 'fede.jpg', 'rb'))
+    bot.send_photo( cid, open( './imagenes/fede.jpg', 'rb'))
     
 @bot.message_handler(commands=['welcome']) 
 def command_welcome(m): 
     cid = m.chat.id 
-    bot.send_photo( cid, open( 'welcome.jpg', 'rb'))
+    bot.send_photo( cid, open( './imagenes/welcome.jpg', 'rb'))
 
 @bot.message_handler(commands=['tabla']) 
 def command_tabla(m): 
     cid = m.chat.id 
-    bot.send_photo( cid, open( 'tabla.jpg', 'rb'))
+    bot.send_photo( cid, open( './imagenes/tabla.jpg', 'rb'))
 
 @bot.message_handler(commands=['takataka']) 
 def command_taka(m): 
     cid = m.chat.id 
-    bot.send_photo( cid, open( 'takataka.jpg', 'rb'))
+    bot.send_photo( cid, open( './imagenes/takataka.jpg', 'rb'))
 
 @bot.message_handler(commands=['ubuntu']) 
 def command_ubuntu(m): 
@@ -187,7 +187,7 @@ def command_ubuntu(m):
 @bot.message_handler(commands=['kill']) 
 def command_kill(m): 
     cid = m.chat.id 
-    bot.send_photo( cid, open( 'rambo.jpg', 'rb'))
+    bot.send_photo( cid, open( './imagenes/rambo.jpg', 'rb'))
 
 @bot.message_handler(commands=['format'])
 def command_format(m): 
