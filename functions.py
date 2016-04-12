486cb5f0b55c6c51cddf8dc40a9dc627ab545646
NeoRanger <elgranpote@gmail.com>
2016-04-11 22:14:19 -0400

    Changing gif document and adding file to .gitignore

--- functions.py
+++ functions.py
@@ -29,7 +29,7 @@ def command_uppercut(m):
 @bot.message_handler(commands=['hadouken']) 
 def command_hadouken(m): 
     cid = m.chat.id 
-    bot.send_photo( cid, open( './imagenes/hadouken.gif', 'rb')) 
+    bot.send_document( cid, open( './imagenes/hadouken.gif', 'rb')) 
 
 @bot.message_handler(commands=['windowsero']) 
 def command_windowsero(m): 
