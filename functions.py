803ceda2b134937e01d3a510b58ba94b64ed26ad
neoranger <neoranger@openmailbox.org>
2016-04-25 14:48:11 +0000

    fixing note function

--- functions.py
+++ functions.py
@@ -361,6 +361,7 @@ def handle_text(m):
     if string_array[0] == "/note" and user.user_id == cid:
         try:
             grabo_nota = (codecs.open("./imagenes/notas.txt", "a", "utf8").write("\n" + string_array[1]))
+            send_message_checking_permission(m, grabo_nota)
         except IndexError:
             bot.send_message( cid, "Argumento invalido. Use /note y lo que quiera grabar. Si no está habilitado para grabar no se moleste en usar el comando" )
 
