9bee8091aab183f8fcfed6e1ea3f6b146a203bc4
NeoRanger <elgranpote@gmail.com>
2016-06-03 23:31:23 -0400

    new command

--- functions.py
+++ functions.py
@@ -228,7 +228,7 @@ def command_acerca(m):
 @bot.message_handler(commands=['help'])
 def command_ayuda(m):
     cid = m.chat.id
-    bot.send_message( cid, "Comandos Disponibles:\n /welcome\n /hola\n /hello\n /add\n /del\n /size\n /ignore\n /kick\n /uppercut\n /hadouken\n /windowsero\n /ubuntu\n /stallman\n /ok\n /yes\n /nsa\n /attack\n /gentoo\n /kde\n /flame\n /tabla\n /vicman\n /deletethat\n /coding\n /nelson\n /spoiler\n /esssta\n /fede\n /litrona\n /vegetta\n /what\n /takataka\n /kill\n /viernes\n /roll\n /time\n /blogroll\n /format\n /fuckyou\n /arch\n /tuxamigos\n /note\n /id\n /acerca\n /support\n /help\n") #
+    bot.send_message( cid, "Comandos Disponibles:\n /welcome\n /hola\n /hello\n /add\n /del\n /size\n /ignore\n /kick\n /uppercut\n /hadouken\n /windowsero\n /ubuntu\n /stallman\n /ok\n /yes\n /nsa\n /attack\n /gentoo\n /kde\n /flame\n /tabla\n /vicman\n /deletethat\n /coding\n /nelson\n /spoiler\n /esssta\n /fede\n /litrona\n /vegetta\n /what\n /takataka\n /kill\n /viernes\n /love\n /roll\n /time\n /blogroll\n /format\n /fuckyou\n /arch\n /tuxamigos\n /note\n /id\n /acerca\n /support\n /help\n") #
 
 @bot.message_handler(commands=['hola'])
 def command_hola(m):
@@ -410,6 +410,11 @@ def command_fuckyou(m):
 def command_arch(m):
     cid = m.chat.id
     bot.send_photo( cid, open( './imagenes/arch.jpg', 'rb'))
+
+@bot.message_handler(commands=['love'])
+def command_love(m):
+    cid = m.chat.id
+    bot.send_photo( cid, open( './imagenes/love.jpg', 'rb'))
 
 @bot.message_handler(commands=['feed'])
 def command_feed(m):
