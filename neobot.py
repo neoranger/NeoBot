e4cf18cab9461845b7d9e0248c38f6fad3775f11
NeoRanger <neoranger@openmailbox.org>
2017-01-10 15:23:42 -0300

    deleted:    download_function.py 	modified:   neobot.py 	deleted:    torrents.py 	deleted:    weather.py

--- neobot.py
+++ neobot.py
@@ -1,13 +1,13 @@
 # -*- coding: utf-8 -*-
 import functions
-#import download_function
 
 #Listener
 def listener(messages):
     for m in messages:
         cid = m.chat.id
+        title = m.chat.title
         if m.content_type == 'text':
-            print ("[" + str(cid) + "]: " + m.text)
+            print ("[" + str(title) + "]: " + m.text)
  
 functions.bot.set_update_listener(listener)
 #############################################
@@ -15,4 +15,3 @@ functions.bot.set_update_listener(listener)
 #Bot starts here
 print('Bot Started')
 functions.bot.polling(none_stop=True)
-#download_funtion.polling(none_stop=True)
